ba10ffd78bd9b6cecbb07e09b2364d870890d06d
hamzaAlmuhisen <halmuhen@gmail.com>
2023-05-24 10:10:12 +0000

    resubmit

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const tax = 0.075;
 const arrayEmp = [];
 
 
-function uniqueId() {
-    return lastId++;
-}
-
 function RandSalary(level) {
     let salary
     salary = MinSalary(level) + LessThan500();
@@ -35,25 +31,36 @@ function MinSalary(level) {
 
 
 
-function Employee(fullName, department, level,img) {
+function Employee(fullName, department, level, img) {
 
-    this.id = uniqueId(),
+    this.id = "0",
         this.fullName = fullName,
         this.department = department,
         this.level = level,
         this.image = img,
         this.salary = RandSalary(level),
+        this.uniqueId = function () {
+            this.id = lastId++;
+        },
+        this.RandSalary = function () {
 
-        arrayEmp.push(this);
+            this.salary = MinSalary(this.level) + LessThan500();
+
+            //calculate the net salary
+            this.salary = Math.floor(this.salary - tax * this.salary);
+        };
+    this.RandSalary();
+    this.uniqueId();
+    arrayEmp.push(this);
 }
 
-let Ghazi = new Employee("Ghazi Samer", "Administration", "Senior","/");
+let Ghazi = new Employee("Ghazi Samer", "Administration", "Senior", "/");
 let Lana = new Employee("Lana Ali", "Finance", "Senior");
-let Tamara = new Employee("Tamara Ayoub", "Administration", "Senior","/");
-let Safi = new Employee("Safi Walid", "Development", "MidSenior","/");
-let Rana = new Employee("Rana Saleh", "Development", "junior","/");
-let Hadi = new Employee("Hadi Ahmad", "Finance", "MidSenior","/");
-
+let Tamara = new Employee("Tamara Ayoub", "Administration", "Senior", "/");
+let Safi = new Employee("Safi Walid", "Development", "MidSenior", "/");
+let Rana = new Employee("Rana Saleh", "Development", "junior", "/");
+let Hadi = new Employee("Hadi Ahmad", "Finance", "MidSenior", "/");
+console.log(arrayEmp);
 Employee.prototype.renderEmployee = function () {
     document.write(`<p> Employee Name: ${this.fullName} </p>`);
     document.write(`<p> Department: ${this.department}</p>`)
