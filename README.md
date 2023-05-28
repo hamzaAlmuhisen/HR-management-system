7999219e135fb13f330afa3ef3256048cfb2092a
hamzaAlmuhisen <halmuhen@gmail.com>
2023-05-28 09:54:31 +0000

    task10

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Task 09
 add header and js code with little styleupdat.
 update app.js 
+
--- app.js
+++ app.js
@@ -2,17 +2,12 @@
 
 let lastId = 1000;
 const tax = 0.075;
-const arrayEmp = [];
+const arrayAdmin = [];
+const Marketing = [];
+const Development = [];
+const Finance = [];
 
 
-function RandSalary(level) {
-    let salary
-    salary = MinSalary(level) + LessThan500();
-
-    //calculate the net salary
-    salary = Math.floor(salary - tax * salary);
-    return salary;
-}
 
 
 function LessThan500() {
@@ -33,40 +28,87 @@ function MinSalary(level) {
 
 function Employee(fullName, department, level, img) {
 
-    this.id = "0",
+    this.id = 1000,
         this.fullName = fullName,
         this.department = department,
         this.level = level,
         this.image = img,
-        this.salary = RandSalary(level),
+        this.salary = 1000,
         this.uniqueId = function () {
             this.id = lastId++;
         },
-        this.RandSalary = function () {
 
+        this.RandSalary = function () {
             this.salary = MinSalary(this.level) + LessThan500();
 
             //calculate the net salary
             this.salary = Math.floor(this.salary - tax * this.salary);
-        };
-    this.RandSalary();
+        }
+
     this.uniqueId();
-    arrayEmp.push(this);
+    this.RandSalary();
+
+
+    if (department == "Administration") arrayAdmin.push(this);
+    if (department == "Finance") Finance.push(this);
+    if (department == "Development") Development.push(this);
+    if (department == "Marketing") Marketing.push(this);
+
 }
 
-let Ghazi = new Employee("Ghazi Samer", "Administration", "Senior", "/");
-let Lana = new Employee("Lana Ali", "Finance", "Senior");
-let Tamara = new Employee("Tamara Ayoub", "Administration", "Senior", "/");
-let Safi = new Employee("Safi Walid", "Development", "MidSenior", "/");
-let Rana = new Employee("Rana Saleh", "Development", "junior", "/");
-let Hadi = new Employee("Hadi Ahmad", "Finance", "MidSenior", "/");
-console.log(arrayEmp);
-Employee.prototype.renderEmployee = function () {
-    document.write(`<p> Employee Name: ${this.fullName} </p>`);
-    document.write(`<p> Department: ${this.department}</p>`)
-    document.write(`<p> Employee salary: ${this.salary}</p><br></br>`)
+let Ghazi = new Employee("Ghazi Samer", "Administration", "Senior", "/assets/Ghazi.png");
+let Tamara = new Employee("Tamara Ayoub", "Marketing", "Senior", "/assets/Tamara.png");
+let Lana = new Employee("Lana Ali", "Finance", "Senior", "/assets/Lana.png");
+let Hadi = new Employee("Hadi Ahmad", "Finance", "MidSenior", "/assets/Hadi.png");
+let Safi = new Employee("Safi Walid", "Development", "MidSenior", "/assets/Safi.png");
+let Omar = new Employee("Omar Zaid", "Development", "Senior", "/assets/Omar.png");
+let Rana = new Employee("Rana Saleh", "Development", "junior", "/assets/Rana.png");
+const section = document.getElementById("section");
+
+
+RenderEmployee(arrayAdmin, "Administration");
+RenderEmployee(Marketing, "Marketing");
+RenderEmployee(Finance, "Finance");
+RenderEmployee(Development, "Development");
+
+
+
+
+function RenderEmployee(arr, name) {
+
+    const sectionName = document.createElement("h1");
+    sectionName.classList.add("sectionText")
+    sectionName.textContent = ` ${name}`;
+    section.appendChild(sectionName);
+    const cardList = document.createElement("div");
+    arr.forEach(elm => {
+
+        const newCard = document.createElement("div");
+        const image = document.createElement("img");
+        const newtext1 = document.createElement("h1");
+        const newtext2 = document.createElement("h1");
+        const newtext3 = document.createElement("h1");
+
+        //class add
+        cardList.classList.add('card-list');
+        newCard.classList.add('card');
+        //class add
+
+        image.setAttribute('src', `${elm.image}`)
+        newtext1.textContent = `Name: ${elm.fullName} - ID: ${elm.id}`;
+        newtext2.textContent = `Department: ${elm.department} - Level: ${elm.level}`;
+        newtext3.textContent = `${elm.salary} $`;
+
+
+        ///appending time ^_^   ////
+        newCard.appendChild(image);
+        newCard.appendChild(newtext1);
+        newCard.appendChild(newtext2);
+        newCard.appendChild(newtext3);
+        cardList.appendChild(newCard);
+        ///appending time ^_^   ////
+
+    });
+    section.appendChild(cardList);
 }
 
-for (let i = 0; i < arrayEmp.length; i++) {
-    arrayEmp[i].renderEmployee();
-}
